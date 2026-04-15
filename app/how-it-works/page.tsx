c66f01af512a464251b159f64b881caccedba923
crynxmartinez <crynxmartinez@github.com>
2026-04-15 12:55:30 +0800

    CRITICAL FIX: Add React import to all client components for JSX support

--- app/how-it-works/page.tsx
+++ app/how-it-works/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import React from 'react'
 import { Header } from "@/components/header"
 import { Footer } from "@/components/footer"
 import { Button } from "@/components/ui/button"
